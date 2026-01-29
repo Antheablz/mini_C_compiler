82de2375a95cb493955835c3b531869bb2ac5b08
Antheablz <agblais@icloud.com>
2026-01-29 12:59:39 -0800

    readng from a file

--- compiler/bin/main.ml
+++ compiler/bin/main.ml
@@ -1,1 +1,20 @@
-let () = print_endline "Hello, World!"
+(* let () = print_endline "Hello, World!" *)
+
+let file = "c_compiler/return_2.c"
+
+
+
+
+
+let () =
+  let ic = open_in file in 
+  try
+    while true do
+      let line = input_line ic in
+      print_endline line;
+      flush stdout
+    done
+  with
+    End_of_file -> close_in ic
+
+
