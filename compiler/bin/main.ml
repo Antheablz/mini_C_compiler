afae8f0920d6f3feb5f1ef15933dd5421c3eac11
Antheablz <agblais@icloud.com>
2026-01-30 16:03:44 -0800

    reading and lexing from file

--- compiler/bin/main.ml
+++ compiler/bin/main.ml
@@ -9,19 +9,62 @@ let get_token_list lexbuf =
   in List.rev (work[])
 
 
-let main =
+(* let main =
   let lexbuf = Lexing.from_string "{ test printf }" in
   let token_list = get_token_list lexbuf in
-  printf "Length: %d\n" (List.length token_list)
+  printf "Length: %d\n" (List.length token_list) *)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 (* let () = print_endline "Hello, World!" *)
 
-(* let current_working_directory = Sys.getcwd ()
+let current_working_directory = Sys.getcwd ()
 let file = current_working_directory ^ "/../return_2.c"
 
+let combined_list: tok list = []
+
 let () =
+  let ic = open_in file in 
+  try
+    while true do
+      let lexbuf = Lexing.from_string (input_line ic) in
+      let token_list = get_token_list lexbuf in
+      (* let _ = combined_list @ token_list in *)
+      let combined_list = (combined_list @ token_list) in
+      (* printf "Length: %d\n" (List.length token_list); *)
+      printf "Length: %d\n" (List.length combined_list);
+      flush stdout
+    done
+  with
+    End_of_file -> close_in ic
+    (* let _ = printf "Length: %d\n" (List.length combined_list); *)
+    let _ = printf "Length: %d\n" (List.length combined_list);
+
+
+
+
+  (* try
+    while true do
+      let line = Lexing.from_channel ic in
+      (* let lexbuf = Lexing.from_string line in *)
+      token_list = get_token_list line
+      (* flush stdout *)
+    done
+  with
+    End_of_file -> close_in ic *)
+
+(* let () =
   let ic = open_in file in 
   try
     while true do
